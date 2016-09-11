bed6a02af10859a807d33a501059e42158d53925
Meni Samet <meni432@gmail.com>
2016-09-11 19:03:57 +0300

    change item data object

--- app/src/main/java/com/menisamet/totake/ItemData.java
+++ app/src/main/java/com/menisamet/totake/ItemData.java
@@ -4,7 +4,37 @@ package com.menisamet.totake;
  * Created by meni on 07/09/16.
  */
 public class ItemData {
-    String name;
-    int numbers;
-    boolean check;
+    private String name;
+    private int numbers;
+    private boolean check;
+
+    public ItemData(String name, int numbers, boolean check) {
+        this.name = name;
+        this.numbers = numbers;
+        this.check = check;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getNumbers() {
+        return numbers;
+    }
+
+    public void setNumbers(int numbers) {
+        this.numbers = numbers;
+    }
+
+    public boolean isCheck() {
+        return check;
+    }
+
+    public void setCheck(boolean check) {
+        this.check = check;
+    }
 }
