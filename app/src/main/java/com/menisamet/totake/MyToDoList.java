9f2d73a05208fad4b56aef91637239a0353d7f86
Meni Samet <meni432@gmail.com>
2016-09-11 12:40:18 +0300

    first commit

---
+++ app/src/main/java/com/menisamet/totake/MyToDoList.java
@@ -0,0 +1,101 @@
+package com.menisamet.totake;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+public class MyToDoList extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_to_do_list);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(MyToDoList.this, Database.static_userListData);
+        mRecyclerView.setAdapter(mAdapter);
+
+    }
+
+}
+//
+
+//
+class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        protected CardView cardView;
+        protected TextView textView;
+
+        public ViewHolder(View view) {
+            super(view);
+            this.cardView = (CardView) view.findViewById(R.id.card_view);
+            this.textView = (TextView) view.findViewById(R.id.info_text);
+        }
+    }
+
+
+    private Context context;
+    private List<ListData> listDatas;
+
+    public MyAdapter (Context context, List<ListData> listDatas){
+        this.context = context;
+        this.listDatas = listDatas;
+    }
+
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent,
+                                                   int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.todo_list_item, null);
+
+        ViewHolder viewHolder = new ViewHolder(view);
+        return viewHolder;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        holder.textView.setText(listDatas.get(position).getListName());
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return (null != listDatas ? listDatas.size() : 0);
+
+
+    }
+}
