de2c51ce46bf1bffdd650ba524ea80dd2f1dd95c
tahel <tahelcit@gmail.com>
2016-09-15 17:29:02 +0300

    tahel commit

--- app/src/main/java/com/menisamet/totake/ListOfItemActivity.java
+++ app/src/main/java/com/menisamet/totake/ListOfItemActivity.java
@@ -1,11 +1,17 @@
 package com.menisamet.totake;
 
+import android.content.Context;
+import android.database.DataSetObserver;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.List;
 
@@ -22,7 +28,7 @@ public class ListOfItemActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_of_item);
         v=(ListView) findViewById(R.id.listView); //find list from activity
-        a=new ArrayAdapter<ItemData>(this,android.R.layout.simple_list_item_1, android.R.id.text1,itemsToTake);
+        a=new item_adapter(this, itemsToTake);
         v.setAdapter(a);
         v.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
@@ -33,5 +39,27 @@ public class ListOfItemActivity extends AppCompatActivity {
                 Utility.showToast(getApplicationContext(), ob);
             }
         });
+    }
+
+    class item_adapter extends ArrayAdapter<ItemData>{
+
+        public item_adapter(Context context, List<ItemData> items) {
+            super(context, 0, items);
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ItemData itemData=getItem(position);
+            if(convertView==null){
+                convertView= LayoutInflater.from(getContext()).inflate(R.layout.item_data_layout,parent,false);
+            }
+
+            TextView itemName= (TextView)convertView.findViewById(R.id.item_text);
+            TextView itemNumber= (TextView)convertView.findViewById(R.id.number_text);
+
+            itemName.setText(itemData.getName());
+            itemNumber.setText(itemData.getNumbers());
+            return convertView;
+        }
     }
 }
